6d3ddc3f64ec16b8b744f2957d40db3f50ea5347
Loginov Maxim <seniorloginov@yandex.ru>
2023-12-27 21:50:35 +0300

    FirstTaskDone

---
+++ tasks/task_1/loginov_m_c_fr_ch_s/c_fr_ch_s.h
@@ -0,0 +1,20 @@
+//  Copyright 2023 Loginov Maxim
+#ifndef  SOURCE_REP_P_PPC_2023_TASKS_TASK_1_LOGINOV_M_C_FR_CH_S_C_FR_CH_S_H_
+#define  SOURCE_REP_P_PPC_2023_TASKS_TASK_1_LOGINOV_M_C_FR_CH_S_C_FR_CH_S_H_
+
+#include <mpi.h>
+#include <vector>
+#include <string>
+
+int calcFreqSeq(const std::string& str, char target);
+int calcFreqPar(const std::string& str, char target);
+
+#endif  //  SOURCE_REP_P_PPC_2023_TASKS_TASK_1_LOGINOV_M_C_FR_CH_S_C_FR_CH_S_H_
+
+
+
+
+
+
+
+
