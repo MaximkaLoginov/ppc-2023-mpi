f62ef38f104816169c4a42ef6f05d31f1d59e32f
Loginov Maxim <seniorloginov@yandex.ru>
2023-12-27 23:07:10 +0300

    ResultVersion

--- tasks/task_1/loginov_m_c_fr_ch_s/main.cpp
+++ tasks/task_1/loginov_m_c_fr_ch_s/main.cpp
@@ -3,7 +3,7 @@
 #include <gtest/gtest.h>
 #include <mpi.h>
 #include <iostream>
-#include "./c_ch_fr_s.h"
+#include "./c_fr_ch_s.h"
 
 TEST(Parallel_Letter_Frequency_count, Test1) {
     int rankProc = 0;
@@ -101,13 +101,17 @@ TEST(Parallel_Letter_Frequency_count, Test5) {
 
 int main(int argc, char** argv) {
     int result_code = 0;
-
     ::testing::InitGoogleTest(&argc, argv);
     ::testing::TestEventListeners& listeners =
         ::testing::UnitTest::GetInstance()->listeners();
 
     if (MPI_Init(&argc, &argv) != MPI_SUCCESS)
         MPI_Abort(MPI_COMM_WORLD, -1);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank != 0) {
+        delete listeners.Release(listeners.default_result_printer());
+    }
     result_code = RUN_ALL_TESTS();
     MPI_Finalize();
 
